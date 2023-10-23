291c26eb42df61c6f0f52f2d1fc0c0004548cb3e
CompuGuy99 <alex@compuguy.dev>
2023-10-23 16:21:42 -0600

    Got initial build to run with this correctly

--- lib/periodMsg/src/periodMsg.h
+++ lib/periodMsg/src/periodMsg.h
@@ -1,4 +1,4 @@
-#pragma once
+// #pragma once
 
 
 void periodic_message(const struct device *dev);
--- src/main.c
+++ src/main.c
@@ -2,13 +2,16 @@
 #include <device.h>
 #include <devicetree.h>
 #include <can/can_loopback.h>
+#include "periodMsg.h"
 
 void main(void)
 {
     const struct device *can_dev;
+    can_dev = device_get_binding("CAN_0");
+    
     // Set CAN mode
     can_set_mode(can_dev,CAN_LOOPBACK_MODE );
 
-
+    periodic_message(can_dev);
 }
 

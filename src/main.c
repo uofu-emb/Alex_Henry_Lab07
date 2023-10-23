7e5dcbf36f76dc0e86c4965e1f5548ad6a19264d
Henry Cope <henrydaviscope@gmail.com>
2023-10-23 15:57:22 -0600

    started main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
 
 void main(void)
 {
+    const struct device *can_dev;
+    // Set CAN mode
+    can_set_mode(can_dev,can_mode.CAN_LOOPBACK_MODE )
 }

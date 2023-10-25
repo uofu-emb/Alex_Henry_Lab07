5eb6c9cbbab175468f4f6f1260c7d8f672575f14
Henry Cope <henrydaviscope@gmail.com>
2023-10-25 17:48:44 -0600

    fixed parameters

--- src/main.c
+++ src/main.c
@@ -75,7 +75,7 @@ void main(void)
     #ifdef ACTIVITY3
     while(1){
         send_hiprio_message_cont(can_dev);
-        send_loprio_message_periodic(can_dev, 1000);
+        //send_loprio_message_periodic(can_dev, 1000);
     }
 
     #endif 
